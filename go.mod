64a090629ec3103c7006f6177e829f4e4a4cdf46
Alexander Leitner <leitnersalex@gmail.com>
2018-09-11 11:22:33 -0400

    gopherjs generate

--- go.mod
+++ go.mod
@@ -1,6 +1,15 @@
 module github.com/aleitner/pgen
 
 require (
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20180825215210-0210a2f0f73c
+	github.com/kisielk/gotool v1.0.0 // indirect
+	github.com/neelance/astrewrite v0.0.0-20160511093645-99348263ae86 // indirect
+	github.com/neelance/sourcemap v0.0.0-20151028013722-8c68805598ab // indirect
+	github.com/shurcooL/httpfs v0.0.0-20171119174359-809beceb2371 // indirect
+	github.com/spf13/cobra v0.0.3 // indirect
+	github.com/spf13/pflag v1.0.2 // indirect
 	golang.org/x/crypto v0.0.0-20180910181607-0e37d006457b
 	golang.org/x/sys v0.0.0-20180909124046-d0be0721c37e // indirect
+	golang.org/x/tools v0.0.0-20180911133044-677d2ff680c1 // indirect
 )
--- main.go
+++ main.go
@@ -3,35 +3,15 @@ package main
 import (
 	"crypto/sha512"
 	"encoding/hex"
-	"flag"
 	"fmt"
-	"syscall"
 
-	"golang.org/x/crypto/ssh/terminal"
+	"github.com/gopherjs/gopherjs/js"
 )
 
 func main() {
-	salt := flag.String("salt", "", "salt for generating pass")
-	service := flag.String("service", "", "aservice for which pass is being created")
-	modifier := flag.String("modifier", "", "Specific modifier about pass or service")
-	length := flag.Int("length", 32, "Pass length")
-
-	flag.Parse()
-
-	if *service == "" {
-		fmt.Println("Service must be set.")
-		return
-	}
-
-	if *salt == "" {
-		fmt.Println("Your password: ")
-		bytes, _ := terminal.ReadPassword(int(syscall.Stdin))
-		*salt = string(bytes)
-	}
-
-	sha512Hash := generateHash(*service, *modifier, *salt, *length)
-
-	fmt.Printf("Generated password for %s@%s: %s\n", *modifier, *service, sha512Hash)
+	js.Global.Set("pgen", map[string]interface{}{
+		"generateHash": generateHash,
+	})
 }
 
 func generateHash(service, mod, salt string, length int) string {
